38b3218e4b623be3bae4e5ad7eb139f725e84705
vishwajeeta <gs699542@gmail.com>
2024-04-24 05:25:37 +0530

    update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
 echo "Build Start"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+
+# build_files.sh
+pip install -r requirements.txt
+
+# make migrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
+
 echo "Build End"
