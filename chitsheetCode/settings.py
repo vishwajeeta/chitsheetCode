0523c577923a2436366cc548befe8e896976614c
vishwajeeta <gs699542@gmail.com>
2024-04-24 08:15:29 +0530

    trying to deploy to the vercel

--- chitsheetCode/settings.py
+++ chitsheetCode/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-cki%5g1#yckoj28f-m2-e+72%n8^j4z&_7uj9jn#7*ffem53*k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
